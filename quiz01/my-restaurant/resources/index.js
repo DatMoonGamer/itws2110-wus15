7a8f10ce6201899a57e9db473e8941d2b42b9913
DatMoonGamer <101743832+DatMoonGamer@users.noreply.github.com>
2025-10-28 19:16:32 -0400

    implementing list

---
+++ quiz01/my-restaurant/resources/index.js
@@ -0,0 +1,52 @@
+window.onload = function() {
+    // alert('JavaScript is fully loaded.');
+
+    var slideIndex = 0;
+    carousel();
+
+    function carousel() {
+        var i;
+        var x = document.getElementsByClassName("slide");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > x.length) { slideIndex = 1 }
+        x[slideIndex - 1].style.display = "block";
+        setTimeout(carousel, 2000); // Change image every 2 seconds
+    }
+
+}
+
+$(document).ready(function() {
+
+    $.ajax({
+        type: "GET",
+        url: "data/menu.json",
+        dataType: "json",
+        success: function(responseData, status) {
+            var output = "<ul class='vert_menu'>";
+
+            $.each(responseData.menu, function(i, item) {
+                output += '<div class="row"><div class="col-12 col-lg-6 mb-5 mx-auto"><div class="content_box">';
+                output += '<h1>' + item.name + '</h1>';
+                output += '</div></div></div>';
+            });
+
+            output += "</ul>";
+            $('#projectMenuOutput').html(output);
+
+            // tooltip
+            $('.vert_menu li').tooltip();
+        },
+        error: function(msg) {
+            // there was a problem
+            alert("There was a problem: " + msg.status + " " + msg.desc);
+        }
+    });
+
+    $(function() {
+        $(document).tooltip();
+    });
+
+});
